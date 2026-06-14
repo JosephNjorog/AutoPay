23ea6762fc567bb0b5cc8d00ca29155b9b2a0444
JosephNjorog <njorojoe11173@gmail.com>
2026-06-14 14:47:25 +0300

    feat: define IAccount interface for ERC-4337 smart account validateUserOp hook

---
+++ contracts/src/interfaces/IAccount.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./UserOperation.sol";
+
+/**
+ * ERC-4337 IAccount interface.
+ * Every TumaSmartWallet must implement this.
+ */
+interface IAccount {
+    /**
+     * Validate user's signature and nonce.
+     * The entryPoint will call this before executing the UserOperation.
+     *
+     * @param userOp          The UserOperation to validate.
+     * @param userOpHash      The hash of the UserOperation (to sign).
+     * @param missingAccountFunds  Missing funds on the account's deposit in the entrypoint.
+     *                             0 if the account has sufficient funds.
+     *
+     * @return validationData SIG_VALIDATION_FAILED(1) on failure.
+     *                        0 on success.
+     *                        Packed as: `uint48(validAfter) | uint48(validUntil) | uint160(sigFailed)`.
+     */
+    function validateUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData);
+}
